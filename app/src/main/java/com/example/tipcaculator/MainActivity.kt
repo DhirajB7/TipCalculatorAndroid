5b3ffab5fb294d8ae4a47610d640487b40b3a9a5
DHIRAJB7 <dhiraj.basavaraju@gmail.com>
2023-02-27 01:37:14 +0530

    almost done

--- app/src/main/java/com/example/tipcaculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcaculator/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.tipcaculator
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tipcaculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip(){
+        val costInDouble = binding.costOfService.text.toString().toDouble()
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * costInDouble
+
+        if(binding.roundUpSwitch.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
+
     }
 }
